5757e968260539fcceea000a1dcd3b6bb83d2ddc
iambabita <bb.babita.bhattarai@gmail.com>
2026-03-07 22:26:55 +0545

    Snake water gun game

---
+++ snakewatergun.py
@@ -0,0 +1,44 @@
+import random
+print(random.randint(0 , 2))
+print(random.randint(0 , 2))
+print(random.randint(0 , 2))
+print(random.randint(0 , 2))
+print(random.randint(0 , 2))
+print(random.randint(0 , 2))
+print(random.randint(0 , 2))
+print(random.randint(0 , 2))
+
+import random
+def check (comp , user):
+    if comp == user:
+       return 0
+    if (comp ==0 and user == 1):
+       
+      return -1
+    if (comp == 1 and user == 2):
+      return
+    if (comp == 2 and user == 2):
+      return -1
+    return 1
+    
+
+comp = random.randint(0 , 2)
+user = int(input("0 for snake , 1 for water and 2 for gun:\n"))
+
+score = check (comp , user)
+
+print("You:" ,user)
+print("computer:" , comp)
+
+if (score  == 0):
+ print("Draw")
+elif(score  == -1):
+  print("You Lose")
+else:
+  print("You Won")
+ 
+ 
+ 
+
+
+
